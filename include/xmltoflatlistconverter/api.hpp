8fddf52161ceca2d875a134c0ec26df37935895e
Holy Chatter <holychatter@gmail.com>
2023-06-25 20:50:11 +0200

    Add macro.hpp

--- include/xmltoflatlistconverter/api.hpp
+++ include/xmltoflatlistconverter/api.hpp
@@ -1,7 +1,7 @@
 #ifndef XMLTOFLATLISTCONVERTER_API_HPP
 #define XMLTOFLATLISTCONVERTER_API_HPP
 
-#include <onsem/common/exportsymbols/macro.hpp>
+#include "macro.hpp"
 
 #if !defined(SWIG) && defined(xmltoflatlistconverter_EXPORTS)
 # define XMLTOFLATLISTCONVERTER_API SEMANTIC_LIB_API_EXPORTS(xmltoflatlistconverter)
